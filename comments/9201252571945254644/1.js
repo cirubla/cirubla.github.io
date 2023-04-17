88ca05a1881372cdd0eb61994f455daeb914fa34
cirbula <cirubla@protonmail.com>
2023-04-17 18:51:15 +0800

    update

--- comments/9201252571945254644/1.js
+++ comments/9201252571945254644/1.js
@@ -3,6 +3,6 @@
 
   var input = new Uint8Array(comments.length);
   for (var i = 0; i < comments.length; i++) { input[i] = comments.charCodeAt(i);}
-  var output = pako.inflate(input);
-  window.g_commentLoader.onLoad(JSON.parse(new TextDecoder("utf-8").decode(output)));
+  var output = pako.inflate(input,{ to: 'string' });
+  window.g_commentLoader.onLoad(JSON.parse(output));
 })();

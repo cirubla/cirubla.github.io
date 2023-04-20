67c469efbec2dea9fd097ddbf8241363cc601c54
cirbula <cirubla@protonmail.com>
2023-04-20 18:52:41 +0800

    update

---
+++ comments/4441963658243277677/2.js
@@ -0,0 +1,12 @@
+(function() 
+{
+  var comments = getData();
+  (window && window.g_commentLoader) && window.g_commentLoader.onLoad({feed: {entry: comments}});
+
+  function getData() 
+  {
+    return data = [
+      {"gd$etag":"W/\"A0EFQnY6eSp7ImA9Xx5SGUs.\"","id":{"$t":"tag:blogger.com,1999:blog-5235590154125226279.post-8852021669400553220"},"published":{"$t":"2023-04-20T13:46:53.811+08:00"},"updated":{"$t":"2023-04-20T13:46:53.811+08:00"},"app$edited":{"xmlns$app":"http://www.w3.org/2007/app","$t":"2023-04-20T13:46:53.811+08:00"},"title":{"$t":"不得不感叹zg的舆论控制，对他们政权有害因素真的是全力抹杀"},"content":{"type":"html","$t":"不得不感叹zg的舆论控制，对他们政权有害因素真的是全力抹杀"},"link":[{"rel":"edit","type":"application/atom+xml","href":"https://www.blogger.com/feeds/5235590154125226279/4441963658243277677/comments/default/8852021669400553220?v=2"},{"rel":"self","type":"application/atom+xml","href":"https://www.blogger.com/feeds/5235590154125226279/4441963658243277677/comments/default/8852021669400553220?v=2"},{"rel":"alternate","type":"text/html","href":"https://program-think.blogspot.com/2011/02/jasmine-revolution-227-notice.html?showComment=1681969613811#c8852021669400553220","title":""}],"author":[{"name":{"$t":"Anonymous"},"email":{"$t":"noreply@blogger.com"},"gd$image":{"rel":"http://schemas.google.com/g/2005#thumbnail","width":"16","height":"16","src":"https://img1.blogblog.com/img/blank.gif"}}],"thr$in-reply-to":{"xmlns$thr":"http://purl.org/syndication/thread/1.0","href":"https://program-think.blogspot.com/2011/02/jasmine-revolution-227-notice.html","ref":"tag:blogger.com,1999:blog-5235590154125226279.post-4441963658243277677","source":"http://www.blogger.com/feeds/5235590154125226279/posts/default/4441963658243277677","type":"text/html"},"gd$extendedProperty":[{"name":"blogger.itemClass","value":"pid-357517073"},{"name":"blogger.displayTime","value":"2023年4月20日 13:46:00"}]},
+    ]
+  }
+})();

13102cd43bb73e07b2305cfe840da8994613caec
cirbula <cirubla@protonmail.com>
2023-04-21 18:35:59 +0800

    update

---
+++ comments/6979374222964590510/2.js
@@ -0,0 +1,12 @@
+(function() 
+{
+  var comments = getData();
+  (window && window.g_commentLoader) && window.g_commentLoader.onLoad({feed: {entry: comments}});
+
+  function getData() 
+  {
+    return data = [
+      {"gd$etag":"W/\"CEUNSXs4fSp7ImA9Xx5REE0.\"","id":{"$t":"tag:blogger.com,1999:blog-5235590154125226279.post-3434906018678790706"},"published":{"$t":"2023-04-20T22:51:38.535+08:00"},"updated":{"$t":"2023-04-20T22:51:38.535+08:00"},"app$edited":{"xmlns$app":"http://www.w3.org/2007/app","$t":"2023-04-20T22:51:38.535+08:00"},"title":{"$t":"希望这个昏庸、无能、愚蠢、邪恶、自私、贪婪的体制赶紧完蛋，希望文明早日降临这片土地"},"content":{"type":"html","$t":"希望这个昏庸、无能、愚蠢、邪恶、自私、贪婪的体制赶紧完蛋，希望文明早日降临这片土地"},"link":[{"rel":"edit","type":"application/atom+xml","href":"https://www.blogger.com/feeds/5235590154125226279/6979374222964590510/comments/default/3434906018678790706?v=2"},{"rel":"self","type":"application/atom+xml","href":"https://www.blogger.com/feeds/5235590154125226279/6979374222964590510/comments/default/3434906018678790706?v=2"},{"rel":"alternate","type":"text/html","href":"https://program-think.blogspot.com/2013/04/more-supervision-less-thankfulness.html?showComment=1682002298535#c3434906018678790706","title":""}],"author":[{"name":{"$t":"Anonymous"},"email":{"$t":"noreply@blogger.com"},"gd$image":{"rel":"http://schemas.google.com/g/2005#thumbnail","width":"16","height":"16","src":"https://img1.blogblog.com/img/blank.gif"}}],"thr$in-reply-to":{"xmlns$thr":"http://purl.org/syndication/thread/1.0","href":"https://program-think.blogspot.com/2013/04/more-supervision-less-thankfulness.html","ref":"tag:blogger.com,1999:blog-5235590154125226279.post-6979374222964590510","source":"http://www.blogger.com/feeds/5235590154125226279/posts/default/6979374222964590510","type":"text/html"},"gd$extendedProperty":[{"name":"blogger.itemClass","value":"pid-357517073"},{"name":"blogger.displayTime","value":"2023年4月20日 22:51:00"}]},
+    ]
+  }
+})();
